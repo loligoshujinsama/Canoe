0a009001819675c7323a593f9d189feb4ad8696a
Curfewat6 <75602956+Curfewat6@users.noreply.github.com>
2023-10-10 23:06:51 +0800

    Update bar_graph.py

--- 101023/bar_graph.py
+++ 101023/bar_graph.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import openpyxl
 import plotly.graph_objects as go
-# import flight_scraper
 from datetime import *
 
-# MONTH_DICTIONARY = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct',
-#                     11: 'Nov', 12: 'Dec'}
+TAB = '    '
+HTML_GRAPH = r"C:\Users\Lucas\PycharmProjects\ICT1002_Assignment\venv\bar_graphtest.html"  # CHANGE THIS
 
 
 def custom_median(values):
@@ -21,44 +20,39 @@ def custom_median(values):
     return sorted_prices[n // 2 - 1]  # Return the lower middle value e.g.: [1,2,3,4] --> 2
 
 
-def read_excel_file(dataframe):
-    """
-    This function takes in a dataframe as a parameter and groups them by airline
-    It returns the sorted dataframe as well as the miniumum and maximum value
-    """
-    # dataframe['Price'] = pd.to_numeric(dataframe['Price'].str.replace('$', '').str.replace(',', ''))
-    return dataframe.groupby('Airline')['Price'].apply(custom_median).reset_index()
-
-
 def bargraph(df, cheap, expensive):
     """
-    This function takes in a dataframe and
+    This function takes in a dataframe and 
     plots a horizontal bar graph for a given month
     where the x axis is the price of the flight
     and the y axis indicates the type of airline
+    additional information such as the cheapest and
+    most expensive flight is also provided.
     """
-
+    
     fig = go.Figure()
     add_min_max(df, cheap, expensive)
     cheap_prices = df['min']
     expensive_prices = df['max']
-    annotations = ['min', 'max']
     fig.add_trace(go.Bar(
         y=df['Airline'],
         x=df['Price'],
-        textposition="none",
         orientation='h',
     ))
-
-    fig.update_traces(text=df['min'],
-                      textposition='inside')
+    #This block of code annotates on each bar the cheapest & most expensive flight for each airline
+    fig.update_traces(
+        text=TAB + TAB + 'Cheapest flight: $' + cheap_prices + TAB + 'Most expensive flight: $' + expensive_prices,
+        textposition='inside',
+        insidetextanchor='start'
+    )
+    #This block of code labels the x, y axis and the graph
     fig.update_layout(
         title=f'Airline average prices',
-        xaxis_title='Price',
+        xaxis_title='Price ($)',
         yaxis_title='Airline',
     )
 
-    fig.write_html(r"C:\Users\brand\PycharmProjects\pythonProject\091023\091023\static\bar_graphtest.html")
+    fig.write_html(HTML_GRAPH)
 
 
 def add_min_max(dataframe, cheapest, expensive):
@@ -67,27 +61,26 @@ def add_min_max(dataframe, cheapest, expensive):
     """
     cheap_list = str(cheapest).split()
     expensive_list = str(expensive).split()
-    cheap_prices = [number for number in cheap_list if number.isdigit()]
-    expensive_prices = [number for number in expensive_list if number.isdigit()]
-    dataframe['min'] = cheap_prices
-    dataframe['max'] = expensive_prices
-
-    return dataframe
+    dataframe['min'] = [number for number in cheap_list if number.isdigit()]
+    dataframe['max'] = [number for number in expensive_list if number.isdigit()]
 
 
 def plot_bargraph(data):
     """
-    This function will only run if and only if it is NOT IMPORTED as a module.
+    This function outlines the logic behind plotting the bargraph
     """
+    airline_data_median = data.groupby('Airline')['Price'].apply(custom_median).reset_index()
     airline_data = data.groupby('Airline')['Price']
-    data_frame = read_excel_file(data)
 
-    bargraph(data_frame, airline_data.min(), airline_data.max())
+    bargraph(airline_data_median, airline_data.min(), airline_data.max())
 
-# df = pd.read_excel(r'C:\Users\Lucas\PycharmProjects\ICT1002_Assignment\venv\scraped.xlsx', engine='openpyxl')
+
+# Anything below this comment is plainly for debugging purposes ONLY :) 
+
+# df = pd.read_excel(r'C:\Users\Lucas\PycharmProjects\ICT1002_Assignment\venv\101023\scraped.xlsx', engine='openpyxl')
 # pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.width', 2000)
 # pd.set_option('display.float_format', '{:20,.2f}'.format)
 # pd.set_option('display.max_colwidth', None)
-# plot_bargraph(df, 10)
+# plot_bargraph(df)
