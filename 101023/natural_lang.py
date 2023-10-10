eed1ed59eff8aafa6d6a88e4f48bde26fca129e1
loligoshujinsama <57374468+loligoshujinsama@users.noreply.github.com>
2023-10-11 02:35:53 +0800

    Update natural_lang.py

--- 101023/natural_lang.py
+++ 101023/natural_lang.py
@@ -19,7 +19,6 @@ def pad_list(dictionary):
     maxNo = []
     for i in dictionary:
         maxNo.append(len(dictionary[i]))
-        
     for key, value in dictionary.items():
         if len(value) < max(maxNo):
             for i in range(len(value),max(maxNo)):
@@ -56,74 +55,72 @@ def sentiment_analyze(text):
     else:
         return 1
 
-def initiateNLP(dict):
+def initiateNLP(dict, airline_name):
     df = pda.DataFrame(pad_list(fetchAirlineReview(dict)[0]))
-    unique_airlines = fetchAirlineReview(dict)[1]
     emotion_dict = {}
     
     # get the emotion dictionary ready
-    emotion_file = open(r"C:\Users\brand\PycharmProjects\pythonProject\091023\091023\emotion.txt",'r',encoding='utf-8')
+    emotion_file = open(r"H:\University\Y1T1\1002_Programming\project\101023\emotion.txt",'r',encoding='utf-8')
     emotion_dict = emotion_sanitize(emotion_file,emotion_dict)
     emotion_file.close()
 
-    # # loop for all reviews in Skytrax dataFrame  
-    for airline_name in unique_airlines:
-        cleaned_text = ""
-        temp_emotion_list = []
-        words_score_dict = {}
-        score = 0
-        moods_list = []
-        print(f"Working on {airline_name} ...")
-        try:
-            for i in range(len(df[airline_name])):
-                # get the review of index i
-                text = str(df[airline_name][i])
+    # for airline_name in unique_airlines:
+    cleaned_text = ""
+    temp_emotion_list = []
+    words_score_dict = {}
+    score = 0
+    moods_list = []
+    print(f"Working on {airline_name} ...")
+    try:
+        for i in range(len(df[airline_name])):
+            # get the review of index i
+            text = str(df[airline_name][i])
+            
+            # step 1: let's clean the text and assign cleaned list to dataFrame 
+            cleaned_text= sentiment_clean_text(text)
+            
+            #Step 2: sentiment Analysis
+            score = sentiment_analyze(cleaned_text)
+            moods_list.append(score)
 
-                # step 1: let's clean the text and assign cleaned list to dataFrame 
-                # simple clean 
-                cleaned_text= sentiment_clean_text(text)
+            # Step 3: advanced clean for emotions
+            cleaned_text_list = emotion_tokenise(cleaned_text)
+            df[airline_name][i] = cleaned_text_list
+            
+            # Step 4: emotion list builder
+            for word in emotion_dict.keys():
+                if word in cleaned_text_list:
+                    temp_emotion_list.append(emotion_dict[word])   
+            
+        '''
+        Bar graph:
+        fig, ax = plt.subplots()
+        ax.bar(mood, count)
+        fig.autofmt_xdate()
+        fig.set_figwidth(20)
+        fig.set_figheight(10)
+        plt.xlabel('Emotions')
+        plt.ylabel('Frequency')
+        '''
 
-                #Step 2: sentiment Analysis
-                score = sentiment_analyze(cleaned_text)
-                moods_list.append(score)
+        words_score_dict = Counter(temp_emotion_list) 
+        big_emotions = list((key, value) for (key, value) in words_score_dict.items())
+        mood = []
+        count = []
+        for key, value in big_emotions:
+            mood.append(key)
+            count.append(value)
 
-                # Step 3: advanced clean for emotions
-                cleaned_text_list = emotion_tokenise(cleaned_text)
-                df[airline_name][i] = cleaned_text_list
-
-                # Step 4: emotion list builder
-                for word in emotion_dict.keys():
-                    if word in cleaned_text_list:
-                        temp_emotion_list.append(emotion_dict[word])   
-
-            '''
-            Bar graph:
-            fig, ax = plt.subplots()
-            ax.bar(mood, count)
-            fig.autofmt_xdate()
-            fig.set_figwidth(20)
-            fig.set_figheight(10)
-            plt.xlabel('Emotions')
-            plt.ylabel('Frequency')
-            '''
-
-            words_score_dict = Counter(temp_emotion_list) 
-            big_emotions = list((key, value) for (key, value) in words_score_dict.items())
-            mood = []
-            count = []
-            for key, value in big_emotions:
-                mood.append(key)
-                count.append(value)
-
-            wordcloud = WordCloud(background_color='black', width=800, height=500, random_state=21, max_font_size=110).generate_from_frequencies(words_score_dict)
-            plt.figure(figsize=(10, 7)) 
-            plt.imshow(wordcloud, interpolation="bilinear") 
-            plt.axis('off') 
-            plt.savefig(f'{airline_name}.png', dpi=300, bbox_inches='tight')
-            print(f"{airline_name}.png created")
-        except Exception:
-            print(f"{airline_name} has no existing reviews")
-            pass
+        wordcloud = WordCloud(background_color='black', width=800, height=500, random_state=21, max_font_size=110).generate_from_frequencies(words_score_dict)
+        plt.figure(figsize=(10, 7)) 
+        plt.imshow(wordcloud, interpolation="bilinear") 
+        plt.axis('off') 
+        plt.savefig(f'static/{airline_name}.png', dpi=300, bbox_inches='tight')
+        print(f"{airline_name}.png created")
+    except Exception as e:
+        print(e)
+        print(f"{airline_name} has no existing reviews")
+        pass
 
 if __name__ == "__main__":
     initiateNLP(dict)
