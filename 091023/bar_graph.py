8948ccec81fbc2323d59f16e69c4a4b79087d523
Curfewat6 <75602956+Curfewat6@users.noreply.github.com>
2023-10-10 16:45:57 +0800

    Update bar_graph.py

--- 091023/bar_graph.py
+++ 091023/bar_graph.py
@@ -1,34 +1,9 @@
 import pandas as pd
 import openpyxl
 import plotly.graph_objects as go
+#import flight_scraper
 from datetime import *
-
 MONTH_DICTIONARY={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
-
-
-"""
-+=+=+=+=+=+=IMPORTANT+=+=+=+=+=+=
-Ensure that the month is an INTEGER before using any of the functions in this module :D
-"""
-
-
-def filter_month(dataframe, month):
-    """
-    This function changes the date format to only include the month as an integer.
-    e.g.: [2023-10-23 ---> 10] ; [2023-01-12] ---> 1] ; [2024-09-29 ---> 9]
-    After which it will only keep the rows relevant to the specified month in the params.
-    e.g.: month == 9 ; returned df will ONLY contain rows with the number 9 in the date column
-    """
-
-    df = dataframe
-    format = '%Y-%m-%d'                                                     # Specify a time format
-    for i in range(len(dataframe['Date'])):
-        try:
-            month_in_number = datetime.strptime(df['Date'][i], format)
-            df.Date[df.Date == df['Date'][i]] = month_in_number.month       # Change 23-01-12 to 1
-        except: 
-            pass
-    return df[df['Date'] == month]                                          # Keep data relevant to the specified month
 
 
 def custom_median(values):
@@ -46,20 +21,13 @@ def custom_median(values):
 
 def read_excel_file(dataframe, month):
     """
-    This function takes in the excel file as a parameter and will
-    convert it into a dataframe using the pandas module
+    This function takes in a dataframe as a parameter and groups them by airline
+    It returns the sorted dataframe as well as the miniumum and maximum value
     """
-
-    # dataframe = pd.read_excel(EXCEL_FILE, engine='openpyxl')
-    print(dataframe)
-    # dataframe['Price'] = pd.to_numeric(dataframe['Price'].str.replace('$', '').str.replace(',', ''))    # Removes the $ from $100
-
-    # median_prices_dataframe = dataframe.groupby('airline')['Price'].apply(custom_median).reset_index()  # Consolidate median data
-    working_df = filter_month(dataframe, month)
-    return working_df.groupby('Airline')['Price'].apply(custom_median).reset_index()
+    return dataframe.apply(custom_median).reset_index()
     
 
-def bargraph(df,month):
+def bargraph(df, cheap, expensive):
     """
     This function takes in a dataframe and 
     plots a horizontal bar graph for a given month
@@ -68,31 +36,57 @@ def bargraph(df,month):
     """
 
     fig = go.Figure()
-    displaying_month = MONTH_DICTIONARY[month]
+    add_min_max(df, cheap, expensive)
+    cheap_prices = df['min']
+    expensive_prices = df['max']
+    annotations = ['min', 'max']
     fig.add_trace(go.Bar(
         y=df['Airline'],
         x=df['Price'],
+        textposition = "none",
         orientation='h',
     ))
 
+
+    fig.update_traces(text=df['min'], 
+        textposition='inside')
     fig.update_layout(
-        title=f'Price by airline for the month of {displaying_month}',
+        title=f'Airline average prices',
         xaxis_title='Price',
         yaxis_title='Airline',
     )
-    # fig.show()              #This line of code pops up the graph. It may be redundant if you want to save as html
-    fig.write_html(r"H:\University\Y1T1\1002_Programming\project\10-7\static\bar_graphtest.html")
+    
+    fig.write_html(r"C:\Users\Lucas\PycharmProjects\ICT1002_Assignment\venv\bar_graphtest.html")
+
+
+def add_min_max(dataframe, cheapest, expensive):
+    """
+    This function appends the cheapest and most expensive flight to the dataframe
+    """
+    cheap_list = str(cheapest).split()
+    expensive_list = str(expensive).split()
+    cheap_prices = [number for number in cheap_list if number.isdigit()]
+    expensive_prices = [number for number in expensive_list if number.isdigit()]
+    dataframe['min'] = cheap_prices
+    dataframe['max'] = expensive_prices
+
+    return dataframe
+
 
 def plot_bargraph(data, month):
     """
     This function will only run if and only if it is NOT IMPORTED as a module.
     """
+    airline_data = data.groupby('Airline')['Price']
+    data_frame = read_excel_file(airline_data, month)
 
-    data_frame = read_excel_file(data, 10)
-    bargraph(data_frame, 10)
+    bargraph(data_frame, airline_data.min(), airline_data.max())
 
 
-
-
-#if __name__ == "__main__":
-#    main()
+# df = pd.read_excel(r'C:\Users\Lucas\PycharmProjects\ICT1002_Assignment\venv\scraped.xlsx', engine='openpyxl')
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.width', 2000)
+# pd.set_option('display.float_format', '{:20,.2f}'.format)
+# pd.set_option('display.max_colwidth', None)
+# plot_bargraph(df, 10)
